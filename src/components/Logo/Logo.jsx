c0c6d68cb3361f6d39b31ce404319929e9770c16
Ivana Braz <braz.ivana@gmail.com>
2023-05-11 09:37:15 -0300

    Fixing logo wrap mobile version

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import Do from "./Do";
 
 const Logo = () => {
     return (
-        <div className="justify-center text-center w-full grid gap-0 grid-cols-2 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2">
+        <div className="justify-center text-center w-full grid gap-0 grid-cols-1 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2">
             <Nu/>
             <Do/>
         </div>
