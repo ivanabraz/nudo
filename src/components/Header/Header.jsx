e07c111dcfd0b132c0ec952e99f8d783c7640669
Ivana Braz <braz.ivana@gmail.com>
2023-05-11 00:01:08 -0300

    Responsive logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../Logo/Logo";
 
 const Header = () => {
     return (
-        <div className="relative w-screen h-screen overflow-hidden flex flex-col justify-between p-5">
+        <div className="w-screen h-screen flex flex-col justify-between">
             <Logo/>
             {/* <div className="flex flex-row justify-center">
                 <div className="w-full grid gap-5 grid-cols-6 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-6">
@@ -15,7 +15,7 @@ const Header = () => {
                     <img alt="Imagen 1" src="https://placehold.jp/300x300.png" />
                 </div>
             </div> */}
-            <div className="flex flex-row justify justify-between">
+            <div className="flex flex-row justify justify-between p-5">
                 <a rel="noreferrer" href="http://www.instagram.com/nudobycris" target="_blank" className="text-nudoRed text-xl xs:text-xl sm:text-3xl md:text-4xl lg:text-6xl flex-start">@nudobycris</a>
                 <p className="text-nudoRed text-xl xs:text-xl sm:text-3xl md:text-4xl lg:text-6xl flex-end">nudobycris[at]gmail.com</p>
             </div>
---
+++ src/components/Logo/Nu.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Nu = () => {
+    return (
+        <>
+            <svg className="w-full" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 1032.3 432.9" xmlSpace="preserve">
+                <path class="fill-nudoRed" d="M366.4,33.3v99.9c-1.8,44.2-64.8,44.2-66.6,0C299.7,59.5,239.9,0,166.6,0S33.4,59.5,33.4,133.2v266.4h166.5
+                V333c0-18.4,14.9-33.3,33.3-33.3s33.3,14.9,33.3,33.3v66.6h233.1V33.3H366.4z"/>
+                <path class="fill-nudoRed" d="M815.9,33.3v166.5c0,18.4-14.9,33.3-33.3,33.3s-33.3-14.9-33.3-33.3V33.3H566.2v183.1
+                c-0.1,120.5,98.5,216.8,216.4,216.4c118,0.3,216.6-96,216.4-216.4V33.3H815.9z"/>
+            </svg>
+        </>
+    )
+}
+
+export default Nu;
