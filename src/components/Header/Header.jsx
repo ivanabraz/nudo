4c32440a9615aca0516f5444264b041a0b63bb42
Ivana Braz <braz.ivana@gmail.com>
2023-05-11 10:10:39 -0300

    Uppercase NUDO

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,8 @@ import Logo from "../Logo/Logo";
 
 const Header = () => {
     return (
-        <div className="w-screen h-screen flex flex-col justify-between">
+        <>
+        <div className="w-full h-full flex flex-col">
             <Logo/>
             {/* <div className="flex flex-row justify-center">
                 <div className="w-full grid gap-5 grid-cols-6 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-6">
@@ -15,11 +16,12 @@ const Header = () => {
                     <img alt="Imagen 1" src="https://placehold.jp/300x300.png" />
                 </div>
             </div> */}
-            <div className="flex flex-row justify justify-between p-5">
-                <a rel="noreferrer" href="http://www.instagram.com/nudobycris" target="_blank" className="text-nudoRed text-xl xs:text-xl sm:text-3xl md:text-4xl lg:text-6xl flex-start">@nudobycris</a>
-                <p className="text-nudoRed text-xl xs:text-xl sm:text-3xl md:text-4xl lg:text-6xl flex-end">nudobycris[at]gmail.com</p>
-            </div>
         </div>
+        <footer className="fixed inset-x-0 bottom-0 flex flex-row justify justify-between p-5">
+            <a rel="noreferrer" href="http://www.instagram.com/nudobycris" target="_blank" className="text-nudoRed text-xl xs:text-xl sm:text-3xl md:text-4xl lg:text-6xl flex-start">@nudobycris</a>
+            <p className="text-nudoRed text-xl xs:text-xl sm:text-3xl md:text-4xl lg:text-6xl flex-end">nudobycris[at]gmail.com</p>
+        </footer>
+        </>
     )
 }
 
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,9 @@ import Do from "./Do";
 
 const Logo = () => {
     return (
-        <div className="justify-center text-center w-full grid gap-0 grid-cols-1 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2">
+        <div className="justify-center text-center w-full 
+        grid gap-0 grid-cols-1 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2
+        pt-3 xs:pt-3 sm:pt-3 md:pt-4 lg:pt-5">
             <Nu/>
             <Do/>
         </div>
