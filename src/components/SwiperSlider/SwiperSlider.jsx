93cdbd5bc493ae66401ae48600a3ede1906374e8
Ivana Braz <braz.ivana@gmail.com>
2023-05-17 20:30:13 -0300

    Adding navigation to swiper

--- src/components/SwiperSlider/SwiperSlider.jsx
+++ src/components/SwiperSlider/SwiperSlider.jsx
@@ -6,33 +6,54 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 
-
+// import required modules
+import { Navigation } from "swiper";
 
 const nudoImages = {
     amigurumi: [
         {
-            imgAlt:"Foto amigurimi 1",
-            imgUrl:"https://placehold.co/400",
+            imgAlt:"Foto amigurimi 4",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-4.jpg?raw=true",
+        },
+        {
+            imgAlt:"Foto amigurimi 5",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-5.jpg?raw=true",
+        },
+        {
+            imgAlt:"Foto amigurimi 6",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-6.jpg?raw=true",
+        },
+        {
+            imgAlt:"Foto amigurimi 7",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-7.jpg?raw=true",
+        },
+        {
+            imgAlt:"Foto amigurimi 8",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-8.jpg?raw=true",
+        },
+        {
+            imgAlt:"Foto amigurimi 9",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-9.jpg?raw=true",
+        },
+        {
+            imgAlt:"Foto amigurimi 10",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-10.jpg?raw=true",
+        },
+        {
+            imgAlt:"Foto amigurimi 11",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-11.jpg?raw=true",
         },
         {
             imgAlt:"Foto amigurimi 1",
-            imgUrl:"https://placehold.co/400",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-1.jpg?raw=true",
         },
         {
-            imgAlt:"Foto amigurimi 1",
-            imgUrl:"https://placehold.co/400",
+            imgAlt:"Foto amigurimi 2",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-2.jpg?raw=true",
         },
         {
-            imgAlt:"Foto amigurimi 1",
-            imgUrl:"https://placehold.co/400",
-        },
-        {
-            imgAlt:"Foto amigurimi 1",
-            imgUrl:"https://placehold.co/400",
-        },
-        {
-            imgAlt:"Foto amigurimi 1",
-            imgUrl:"https://placehold.co/400",
+            imgAlt:"Foto amigurimi 3",
+            imgUrl:"https://github.com/ivanabraz/nudo/blob/main/src/images/nudo-3.jpg?raw=true",
         },
     ],
 }
@@ -40,12 +61,12 @@ const nudoImages = {
 const SwiperSlider = () => {
     return (
         <>
-                <Swiper slidesPerView={1}
+                <Swiper slidesPerView={1} navigation={true} modules={[Navigation]}
                 spaceBetween={0} className="mySwiper"
                 breakpoints={{
                     475: {
                         slidesPerView: 1,
-                        spaceBetween: 0,
+                        spaceBetween: 10,
                         },
                     640: {
                         slidesPerView: 3,
